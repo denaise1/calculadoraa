27db2b9f2971abd8e1f0d9e801e0bedbfadbc73f
denaise1 <denise.s.santos13@aluno.senai.br>
2026-05-07 14:39:54 -0300

    Add files via upload

---
+++ subtracao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        {
+            int resultado = numero1 - numero2;
+
+            
+            Console.WriteLine("A subtração é: " + resultado);
+        }
+    }
+}
+}
