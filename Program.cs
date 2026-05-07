f2a7a590465a1a9f198ceb249ba9adc6470cbdf0
denaise1 <denise.s.santos13@aluno.senai.br>
2026-05-07 14:17:14 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        {
+            Console.Write("Digite o primeiro número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            int resultado = numero1 + numero2;
+
+            Console.WriteLine("Resultado: " + resultado);
+        }
+    }
+}
+}
